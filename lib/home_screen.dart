9cd772382b6cb7fefefa37dd406763a895cc5861
Gergis <gargis54321@gmail.com>
2024-02-17 11:10:24 +0200

    initial project add empty home screen .

---
+++ lib/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = 'homescreen';
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
